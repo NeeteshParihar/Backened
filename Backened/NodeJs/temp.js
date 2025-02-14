587071221d2aedfab539eef1002e61d7fde84553
NeeteshParihar <neeteshparihar22@gmail.com>
2025-02-14 21:48:45 +0530

    Add remove method in todo app

--- Backened/NodeJs/temp.js
+++ Backened/NodeJs/temp.js
@@ -1,51 +1,97 @@
 
+const fs = require("fs");
+
+const filePath = `./tasks.json`;
+
+const command = process.argv[2];
+const argument = process.argv[3];
 
 
-const fs = require("fs") ;
 
-const filePath = `./tasks.json` ;
+const loadTasks = () => {
 
-const command = process.argv[2]  ;
-const argument = process.argv[3] ;
+    try {
+        const bufferObject = fs.readFileSync(filePath);
 
+        const JSONStr = bufferObject.toString();
 
-const loadTasks = ()=>{
+        return JSON.parse(JSONStr);
 
-    try{
+    } catch (error) {
 
-        const dataBuffer = fs.readFileSync(filePath) ;
-        const dataJSON = dataBuffer.toString() ;
-        return JSON.parse(dataJSON) ;
-    }catch(error){
-        return [] ;
+        return [];
     }
-    
-    
-}
 
-if(command === "add"){
-    addTask(argument) ;
+
 
 }
 
+const saveTasks = (tasks) => {
+
+    const dataJSON = JSON.stringify(tasks);
+
+    fs.writeFileSync(filePath, dataJSON);
 
 
+}
+
+const addTask = (task) => {
+
+    const Tasks = loadTasks();
+    Tasks.push({ task });
+    saveTasks(Tasks);
+
+    console.log("task added :", task);
+
+}
 
 
+const listTasks = () => {
+
+    const Tasks = loadTasks();
+
+    Tasks.forEach(
+        (task, index) => {
+
+            console.log(`${index + 1} - ${task.task}`)
+
+        }
+    )
 
 
+}
 
+const DeleteTask = (argument)=>{
 
+    let index = Number(argument) -1 ;
 
+    if(Number.isNaN(index)  || index < 0){
+        console.log("index error") ;
+        return ;
+    }
+    const Tasks = loadTasks() ;
 
+    if(Tasks.length <= index ){
+        console.log("Deletion failed : enter within the range of index")  ;
+        return ;
+    }
 
+    const newTasks = Tasks.slice(0,index).concat(Tasks.slice(index+1)) ;
 
+    saveTasks(newTasks) ;
 
+    console.log("deleted task :",  Tasks[index]) ;
+}
 
+if (command === "add") {
+    addTask(argument);
+} else if (command === "list") {
+    listTasks();
+} else if (command === "delete") {
+    
+    DeleteTask(argument) ;
 
-
-
-
-
-
-
+   
+} else {
+    console.log("command not found");
+}
